c41d04eaf80af77d2c3ca2161b49c22ce130ef33
Jason R. Coombs <jaraco@jaraco.com>
2013-12-29 14:25:33 -0500

    Use 'apt' in favor of 'aptitude' as either should be supported.

--- jaraco/apt.py
+++ jaraco/apt.py
@@ -7,7 +7,7 @@ import six
 class PackageName(six.text_type):
     """A package name possibly with other attributes"""
     @classmethod
-    def from_aptitude(cls, name):
+    def from_apt(cls, name):
         automatic = name.endswith('{a}')
         if automatic:
             name = name[:-3]
@@ -15,16 +15,16 @@ class PackageName(six.text_type):
         res.automatic = automatic
         return res
 
-def parse_new_packages(aptitude_output, include_automatic=False):
+def parse_new_packages(apt_output, include_automatic=False):
     """
-    Given the output from an aptitude command, determine which packages are
-    newly-installed.
+    Given the output from an apt or aptitude command, determine which packages
+    are newly-installed.
     """
     pat = r'^The following NEW packages will be installed:[\r\n]+(.*?)[\r\n]\w'
-    matcher = re.search(pat, aptitude_output, re.DOTALL | re.MULTILINE)
+    matcher = re.search(pat, apt_output, re.DOTALL | re.MULTILINE)
     if not matcher: return []
     new_pkg_text = matcher.group(1)
     raw_names = re.findall(r'[\w{}\.+-]+', new_pkg_text)
-    all_packages = list(map(PackageName.from_aptitude, raw_names))
+    all_packages = list(map(PackageName.from_apt, raw_names))
     manual_packages = [pack for pack in all_packages if not pack.automatic]
     return all_packages if include_automatic else manual_packages
