9abb05d62835288a122ebe7f511a842ccc8d4664
Jason R. Coombs <jaraco@jaraco.com>
2019-01-01 10:49:53 -0500

    Feed the hobgoblins (delint).

--- tests/test_apt.py
+++ tests/test_apt.py
@@ -11,14 +11,16 @@ Even more stuff
 Stuff again
 """
 
+
 def test_parse_new_packages():
     pkgs = apt.parse_new_packages(sample_output)
     assert not any('stuff' in pkg.lower() for pkg in pkgs)
     assert 'abc' not in pkgs
     assert pkgs == ['ghi', 'jk', 'lmno', 'xyz', 'g++']
 
+
 def test_parse_new_packages_include_automatic():
     pkgs = apt.parse_new_packages(sample_output, include_automatic=True)
     assert not any('stuff' in pkg.lower() for pkg in pkgs)
-    assert pkgs == ['abc', 'def', 'ghi', 'jk', 'lmno', 'pqwerty', 'xyz',
-        'g++']
+    assert pkgs == [
+        'abc', 'def', 'ghi', 'jk', 'lmno', 'pqwerty', 'xyz', 'g++']
